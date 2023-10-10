f08002abdb510bdbb03161ef8d99a05b694eb56c
FASami <foysal.developer@gmail.com>
2023-10-10 20:31:16 +0600

    tweak: test hard coded user id

--- pages/api/webhook/authorize-request.js
+++ pages/api/webhook/authorize-request.js
@@ -2,34 +2,35 @@ import { decode } from 'next-auth/jwt'
 
 const authorizeRequest = async (req, res) => {
   console.log('Authorize request ')
-  return res.status(200).json({
-    success: true,
-    error: 'true',
-  })
-   const authToken = req.body.headers['mullayan-next-auth-token']
-   if (authToken) {
-     try {
-       const { userId } = await decode({
-         token: authToken,
-         secret: process.env.NEXTAUTH_SECRET,
-       })
 
-       if (userId) {
-         return res.status(200).json({
-           'X-Hasura-User-Id': userId,
-         })
-       }
-       return res
-         .status(403)
-         .json({ success: false, error: 'Unauthorize access' })
-     } catch (error) {
-       return res.status(404).json({ success: false, error: error.message })
-     }
-   } else {
-     return res.status(200).json({
-       success: false,
-       error: 'Unauthorize access',
-     })
-   }
+  return res.status(200).json({
+    'X-Hasura-User-Id': 34,
+  })
+
+  const authToken = req.body.headers['mullayan-next-auth-token']
+  if (authToken) {
+    try {
+      const { userId } = await decode({
+        token: authToken,
+        secret: process.env.NEXTAUTH_SECRET,
+      })
+
+      if (userId) {
+        return res.status(200).json({
+          'X-Hasura-User-Id': userId,
+        })
+      }
+      return res
+        .status(403)
+        .json({ success: false, error: 'Unauthorize access' })
+    } catch (error) {
+      return res.status(404).json({ success: false, error: error.message })
+    }
+  } else {
+    return res.status(200).json({
+      success: false,
+      error: 'Unauthorize access',
+    })
+  }
 }
 export default authorizeRequest
