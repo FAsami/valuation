68df6df454a2ae6905d50cc3b74afd462e46dc8d
FASami <foysal.developer@gmail.com>
2023-12-03 01:39:42 +0600

    feat: added fonts

--- pages/_document.js
+++ pages/_document.js
@@ -33,6 +33,16 @@ export default class MyDocument extends Document {
       <Html>
         <Head />
         <title>মূল্যায়ন</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Bengali:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
         <body>
           <Main />
           <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    display: ['Noto Sans Bengali', 'sans-serif'],
+    body: ['Noto Sans Bengali', 'sans - serif'],
+    extend: {
+      colors: {
+        'v-text': '#151043',
+        'v-text-muted': '#434343',
+        'v-bg': '#27282b',
+      },
+    },
   },
   plugins: [],
 }
